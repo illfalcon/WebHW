714682e5fee59c4242124b699f1f3c3406401c8a
illfalcon <kostasminch@gmail.com>
2018-11-13 16:13:52 +0300

    Revert "added registration and logging in mechanisms"
    This reverts commit ec706f39fbf7a1ace61649a4223b5f223bb090a4.

--- project1/application.py
+++ project1/application.py
@@ -1,10 +1,9 @@
 import os
 
-from flask import Flask, session, render_template, request, redirect, url_for
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -21,54 +20,11 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-def checkIfUserExists(username):
-    user = db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).fetchone()
-    if user == None:
-        return False
-    else:
-        return True
-
-def addUser(name, surname, username, password):
-    db.execute("INSERT INTO users(name, surname, username, password) VALUES "
-               "(:name, :surname, :username, :password)",
-               {"name": name, "surname": surname, "username": username, "password": password})
-    db.commit()
 
 @app.route("/")
 def index():
-    if session.get("user_id") == None:
-        return render_template("index.html")
-    else:
-        return redirect(url_for("books"))
+    return render_template("index.html")
 
-@app.route("/login", methods=["POST"])
-def login():
-    username = request.form.get("username")
-    passwordText = request.form.get("password")
-    #todo: implement security check
-    user = db.execute("SELECT * FROM users WHERE username = :username",
-                      {"username": username}).fetchone()
-    if user == None:
-        return render_template("loginerror.html")
-    elif not check_password_hash(user.password, passwordText):
-        return render_template("loginerror.html")
-    else:
-        session["user_id"] = user.id
-        return redirect(url_for("books"))
-
-@app.route("/register", methods=["POST"])
+@app.route("/register")
 def register():
-    name = request.form.get("name")
-    surname = request.form.get("surname")
-    username = request.form.get("username")
-    passwordText = request.form.get("password")
-    passwordHash = generate_password_hash(passwordText)
-    if checkIfUserExists(username):
-        return render_template("registererror.html")
-    else:
-        addUser(name, surname, username, passwordHash)
-        return redirect(url_for('books'))
-
-@app.route("/books")
-def books():
-    return "Books"
+    return render_template("index.html")
